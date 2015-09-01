c7fa10585421ab46cc33110275acc99fa220679d
William Xie <wxie@cs.utexas.edu>
2015-09-01 18:08:45 -0500

    C++ code working for detecting orange pixels

---
+++ core/python/behaviors/follow_ball.py
@@ -0,0 +1,16 @@
+import memory, pose, commands, cfgstiff, core
+from task import Task
+from state_machine import *
+
+class Ready(Task):
+  def run(self):
+    memory.speech.say("I want ball!")
+    commands.standStraight()
+    if self.getTime() > 3.0:
+      ball = memory.world_objects.getObjPtr(core.WO_BALL)
+      if ball.seen:
+        print("BAAAALLLL Found!")
+        print(ball.imageCenterX)
+        print(ball.imageCenterY)
+
+
