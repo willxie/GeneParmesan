2bd6284580562074c81fc8138ad09304e6270799
Edward Banner <edward.banner@gmail.com>
2015-09-01 21:52:22 -0500

    Print values when the ball is in range
    Closes #4

---
+++ core/python/behaviors/detect_ball.py
@@ -0,0 +1,12 @@
+import memory, pose, commands, cfgstiff, core
+from task import Task
+from state_machine import *
+
+class Ready(Task):
+  def run(self):
+    memory.speech.say("I want ball!")
+    commands.standStraight()
+    if self.getTime() > 3.0:
+      ball = memory.world_objects.getObjPtr(core.WO_BALL)
+      if ball.seen:
+        print("Ball Found at ({}, {})".format(ball.imageCenterX, ball.imageCenterY))
