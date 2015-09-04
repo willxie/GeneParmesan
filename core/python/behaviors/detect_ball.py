737f786802ae82aafe2045bf21ea53e8bf9f3e12
Edward Banner <edward.banner@gmail.com>
2015-09-04 16:16:28 -0500

    Change voice commands

--- core/python/behaviors/detect_ball.py
+++ core/python/behaviors/detect_ball.py
@@ -4,9 +4,10 @@ from state_machine import *
 
 class Ready(Task):
   def run(self):
-    memory.speech.say("I want ball!")
+    memory.speech.say("Where's the ball?")
     commands.standStraight()
     if self.getTime() > 3.0:
       ball = memory.world_objects.getObjPtr(core.WO_BALL)
       if ball.seen:
+        memory.speech.say("I see the ball!")
         print("Ball Found at ({}, {})".format(ball.imageCenterX, ball.imageCenterY))
