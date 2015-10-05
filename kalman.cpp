755dd0d838790e1fd41d7860082b13ad943f470b
Edward Banner <edward.banner@gmail.com>
2015-10-05 14:56:28 -0500

    Get Kalman filter working for 2D canon problem
    Still assuming the control vector and sensor vector are the same dimensionality
    of state vector. Changing these values will have ramifications for the C vector.
    We might be able to get away with keeping all of these vectors the same size, so
    I'm leaving it until it gives us problems.

--- kalman.cpp
+++ kalman.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
 #include <random>
+#include <math.h>
 
 using namespace std;
 using namespace Eigen;
@@ -17,14 +18,13 @@ class KalmanFilter {
     Matrix<double, DIM, DIM> C;
 
     Matrix<double, DIM, DIM> pred_err;
-    Matrix<double, DIM, 1> sensor_err;
+    Matrix<double, DIM, DIM> sensor_err;
 
     //  x = state, sigma = covariance matrix, u = control,, z = observation
     Matrix<double, DIM, 1> x;
     Matrix<double, DIM, 1> x_pred;
     Matrix<double, DIM, DIM> x_err;
     Matrix<double, DIM, DIM> x_err_pred;
-    double K;
 
     KalmanFilter (Matrix<double, DIM, DIM>& A,
     		      Matrix<double, DIM, DIM>& B,
@@ -35,7 +35,7 @@ class KalmanFilter {
 				  Matrix<double, DIM, DIM>& x_err):
         A(A), B(B), C(C),
 		pred_err(pred_err), sensor_err(sensor_err),
-		x_err(x_err), x(x_0) {}
+		x(x_0), x_err(x_err) {}
 
     void update(Matrix<double, DIM, 1> u, Matrix<double, DIM, 1> z) {
         // State estimation
@@ -43,7 +43,8 @@ class KalmanFilter {
         x_err_pred = A * x_err * A.transpose() + pred_err;
 
         // Measurement update
-        K = x_err_pred * C.transpose() * (C * x_err_pred * C.transpose() + sensor_err).inverse();
+        Matrix<double, DIM, DIM> innovation_cov = C * x_err_pred * C.transpose() + sensor_err;
+        Matrix<double, DIM, DIM> K = x_err_pred * C.transpose() * innovation_cov.inverse();
         x_pred = x_pred + K * (z - C * x_pred);
         x_err_pred = (Matrix<double, DIM, DIM>::Identity(DIM, DIM) - K * C) * x_err_pred;
 
@@ -63,74 +64,154 @@ class KalmanFilter {
 };
 
 int main(int argc, char *argv[]) {
-    const int DIM = 1;
+    const int DIM = 4;
 
     Matrix<double, DIM, DIM> A;
     // TODO: B should be DIM x M where M is the dimensionality of the Ccntrol vector u
     Matrix<double, DIM, DIM> B;
     Matrix<double, DIM, DIM> C;
 
-    Matrix<double, DIM, DIM> error_a;
-    Matrix<double, DIM, DIM> error_z;
+    Matrix<double, DIM, DIM> pred_err;
+    Matrix<double, DIM, DIM> sensor_err;
 
     Matrix<double, DIM, 1> x_0;
-    Matrix<double, DIM, DIM> error_x;
+    Matrix<double, DIM, DIM> x_err;
+
+    const double DELTA_T = 0.1;
 
     // Dynamical model
-    A(0,0) = 1; // State is not changing
-    B(0,0) = 0; // No control input
-    C(0,0) = 1; // z is already in state coordinates
+    A << 1, DELTA_T, 0,       0,
+	     0,       1, 0,       0,
+		 0,       0, 1, DELTA_T,
+		 0,       0, 0,       1;
 
-    // Sources of error
-    error_a(0,0) = 0.00001;  // Prediction error
-    error_z(0,0) = 0.1;      // Measurement error
+    // This matrix only makes sense when combined with the u vector
+    B << 0, 0, 0, 0,
+	     0, 0, 0, 0,
+		 0, 0, 1, 0,
+		 0, 0, 0, 1;
 
-    // Initial estimates
-    x_0(0,0) = 3;      // Initial estimate of the state
-    error_x(0,0) = 1;  // Initial state error
+    // No transformation from state to sensor land
+    C = Matrix<double, DIM, DIM>::Identity(DIM, DIM);
+
+    // No prediction error since it's deterministic
+    pred_err = Matrix<double, DIM, DIM>::Zero(DIM, DIM);
+
+    // Some sensor error
+    sensor_err = Matrix<double, DIM, DIM>::Identity(DIM, DIM) * 0.2;
+
+    // Initial guess. Totally wrong (?)
+    x_0 << 0,
+    	   100 * cos(M_PI/4),
+		   500,
+		   100 * sin(M_PI/4);
+
+    // Don't know any better
+    x_err = Matrix<double, DIM, DIM>::Identity(DIM, DIM);
 
     // Initialize the filter
-    KalmanFilter<DIM> filter (A, B, C, error_a, error_z, x_0, error_x);
+    KalmanFilter<DIM> filter (A, B, C, pred_err, sensor_err, x_0, x_err);
 
-    const int NUM_SAMPLES = 60;
-    const double TRUE_VOLTAGE = 1.25;
+    const double GRAVITY = 9.81;
+    Matrix<double, 4, 1> u;
+    u << 0,
+    	 0,
+		 -0.5 * GRAVITY * DELTA_T * DELTA_T,
+		 -GRAVITY * DELTA_T;
+
+    int NOISE_LEVEL = 30;
+    int muzzle_vel = 100;
+    int angle = 45;
+    int ITERATIONS = 144;
+
+    // Cannon parameters
+    double x, y, x_new, y_new;
+    x = y = 0;
+
+    double x_vel, y_vel, x_vel_new, y_vel_new;
+    x_vel = muzzle_vel * cos(angle * M_PI/180);
+    y_vel = muzzle_vel * sin(angle * M_PI/180);
 
     // Gaussian number generator
-    std::default_random_engine generator;
-    std::normal_distribution<double> distribution(TRUE_VOLTAGE, 0.25);
+	std::default_random_engine generator;
 
-    double true_voltages[NUM_SAMPLES];
-    double measures[NUM_SAMPLES];
-    double kalmans[NUM_SAMPLES];
-    for (int i = 0; i < NUM_SAMPLES; ++i) {
-    	// Generate a reading from the voltage meter
-        double measured = distribution(generator);
+    double true_xs[ITERATIONS];
+    double true_ys[ITERATIONS];
 
-        measures[i] = measured;
-        true_voltages[i] = TRUE_VOLTAGE;
-        Matrix<double, DIM, 1> x = filter.getState();
-        kalmans[i] = x(0,0);
+    double noisy_xs[ITERATIONS];
+    double noisy_ys[ITERATIONS];
 
-        // Send it into the Kalman Filter
-        Matrix<double, DIM, DIM> z;
-        z(0,0) = measured;
-        filter.update(Matrix<double, DIM, DIM>::Zero(DIM, DIM), z);
+    double kalman_xs[ITERATIONS];
+    double kalman_ys[ITERATIONS];
+
+    Matrix<double, DIM, 1> z;
+    for (int i = 0; i < ITERATIONS; i++) {
+    	// True location of ball
+    	true_xs[i] = x;
+    	true_ys[i] = y;
+
+    	// Kalman estimate of ball's position
+    	Matrix<double, DIM, 1> kalman_state = filter.getState();
+    	kalman_xs[i] = kalman_state(0,0);
+    	kalman_ys[i] = kalman_state(2,0);
+
+    	// Generate noisy estimate
+		std::normal_distribution<double> x_dist(x, NOISE_LEVEL);
+		std::normal_distribution<double> y_dist(y, NOISE_LEVEL);
+		noisy_xs[i] = x_dist(generator);
+		noisy_ys[i] = y_dist(generator);
+
+		// Update ball's true position
+    	x_new = x + x_vel*DELTA_T;
+    	y_new = y + y_vel*DELTA_T - 0.5*GRAVITY*DELTA_T*DELTA_T;
+
+    	x_vel_new = x_vel;
+    	y_vel_new = y_vel - GRAVITY*DELTA_T;
+
+    	x = x_new;
+    	y = y_new;
+    	x_vel = x_vel_new;
+    	y_vel = y_vel_new;
+
+    	// Give the new measurement and control input to the filter and let it update its belief
+    	z << noisy_xs[i],
+    		 x_vel_new,
+			 noisy_ys[i],
+			 y_vel_new;
+    	filter.update(u, z);
     }
 
-    cout << "measured = [";
-    for (auto measure : measures)
-    	cout << measure << ", ";
-    cout << "]" << endl;
+	cout << "xs = [";
+	for (auto x : true_xs)
+		cout << x << ", ";
+	cout << "]" << endl;
 
-    cout << "true_voltages = [";
-    for (auto true_voltage : true_voltages)
-    	cout << true_voltage << ", ";
-    cout << "]" << endl;
+	cout << "ys = [";
+	for (auto y : true_ys)
+		cout << y << ", ";
+	cout << "]" << endl;
+	cout << endl;
 
-    cout << "kalmans = [";
-    for (auto kalman : kalmans)
-    	cout << kalman << ", ";
-    cout << "]" << endl;
+	cout << "noisy_xs = [";
+	for (auto noisy_x : noisy_xs)
+		cout << noisy_x << ", ";
+	cout << "]" << endl;
+
+	cout << "noisy_ys = [";
+	for (auto noisy_y : noisy_ys)
+		cout << noisy_y << ", ";
+	cout << "]" << endl;
+	cout << endl;
+
+	cout << "kalman_xs = [";
+	for (auto kalman_x : kalman_xs)
+		cout << kalman_x << ", ";
+	cout << "]" << endl;
+
+	cout << "kalman_ys = [";
+	for (auto kalman_y : kalman_ys)
+		cout << kalman_y << ", ";
+	cout << "]" << endl;
 
     return 0;
 }
