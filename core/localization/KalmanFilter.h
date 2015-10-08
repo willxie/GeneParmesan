5c4e6af4020e4992189a9d3bfcc866018084ed42
William Xie <williamgxie@gmail.com>
2015-10-08 01:28:50 -0500

    Added documentation for KalmanFilter.h

--- core/localization/KalmanFilter.h
+++ core/localization/KalmanFilter.h
@@ -1,10 +1,3 @@
-/*
- * KalmanFilter.h
- *
- *  Created on: Oct 5, 2015
- *      Author: ebanner
- */
-
 #ifndef TRUNK_CORE_LOCALIZATION_KALMANFILTER_H_
 #define TRUNK_CORE_LOCALIZATION_KALMANFILTER_H_
 
@@ -13,6 +6,7 @@
 using namespace std;
 using namespace Eigen;
 
+// Kalman filter parameters
 const int DIM_X = 6;
 const int DIM_U = 4;
 const int DIM_Z = 2;
@@ -28,15 +22,17 @@ class KalmanFilter {
     Matrix<double, DIM_X, DIM_U> B;
     Matrix<double, DIM_Z, DIM_X> C;
 
+    // State transition error and sensation error (pred_error = R, sensor_err = Q)
     Matrix<double, DIM_X, DIM_X> pred_err;
     Matrix<double, DIM_Z, DIM_Z> sensor_err;
 
-    // x_err = sigma
+    // State and covariance terms (x_err = sigma)
     Matrix<double, DIM_X, 1> x;
     Matrix<double, DIM_X, 1> x_pred;
     Matrix<double, DIM_X, DIM_X> x_err;
     Matrix<double, DIM_X, DIM_X> x_err_pred;
 
+    // Constructors
     KalmanFilter () {}
 
     KalmanFilter (Matrix<double, DIM_X, DIM_X> A,
@@ -50,6 +46,7 @@ class KalmanFilter {
 		pred_err(pred_err), sensor_err(sensor_err),
 		x(x_0), x_err(x_err) {}
 
+    // Update using both control and sensor values
     void update(Matrix<double, DIM_U, 1> u, Matrix<double, DIM_Z, 1> z) {
         // State estimation
         x_pred = (A * x) + (B * u);
@@ -67,6 +64,7 @@ class KalmanFilter {
         x_err = x_err_pred;
     }
 
+    // Update using solely state transition prediction with no control and sensation
     void updateWithoutAnything() {
         // State estimation
         x_pred = (A * x);
@@ -99,30 +97,22 @@ class ExtendedKalmanFilter {
     Matrix<double, DIM_X, DIM_X> G;
     Matrix<double, DIM_Z, DIM_X> H;
 
+    // State transition error and sensation error (pred_error = R, sensor_err = Q)
     Matrix<double, DIM_X, DIM_X> pred_err;
     Matrix<double, DIM_Z, DIM_Z> sensor_err;
 
-    // x_err = sigma
+    // State and covariance terms (x_err = sigma)
     Matrix<double, DIM_X, 1> x;
     Matrix<double, DIM_X, 1> x_pred;
     Matrix<double, DIM_X, DIM_X> x_err;
     Matrix<double, DIM_X, DIM_X> x_err_pred;
 
+    // Arbitrary functions that model transition and sensation (g(u, x), h(x_pred))
     Matrix<double, DIM_X, 1> (*transitionFunction)(Matrix<double, DIM_X, 1>, Matrix<double, DIM_U, 1>);
     Matrix<double, DIM_Z, 1> (*measurementFunction)(Matrix<double, DIM_X, 1>);
 
+    // Constructors
     ExtendedKalmanFilter () {}
-
-    // ExtendedKalmanFilter (Matrix<double, DIM_X, DIM_X> A,
-    // 		      Matrix<double, DIM_X, DIM_U>& B,
-    //               Matrix<double, DIM_Z, DIM_X>& C,
-    //               Matrix<double, DIM_X, DIM_X>& pred_err,
-	// 			  Matrix<double, DIM_Z, DIM_Z>& sensor_err,
-	// 			  Matrix<double, DIM_X, 1>& x_0,
-	// 			  Matrix<double, DIM_X, DIM_X>& x_err):
-    //     A(A), B(B), C(C),
-	// 	pred_err(pred_err), sensor_err(sensor_err),
-	// 	x(x_0), x_err(x_err) {}
 
     // Calculate the Jacobian matrix based on the input function
     void calculateJacobianX(Matrix<double, DIM_X, 1> (*function)(Matrix<double, DIM_X, 1>, Matrix<double, DIM_U, 1>),
@@ -135,13 +125,15 @@ class ExtendedKalmanFilter {
     		// Add delta to x one dimension at a time
     		Matrix<double, DIM_X, 1> input_temp = input_x;
     		input_temp(i, 0) = input_temp(i, 0) + delta;
+            // Feed modified input to get output
         	Matrix<double, DIM_X, 1> output_bar_temp = (*function)(input_temp, input_u);
-        	// Fill in the ith row.
-        	// The ith row takes the partial derivative of ith element of x
+        	// Fill in the ith column with the derivative.
+        	// The ith column takes the partial derivative of ith element of x
         	G.col(i) = (output_bar_temp - output_bar) / delta;
 
     	}
     }
+
     void calculateJacobianZ(Matrix<double, DIM_Z, 1> (*function)(Matrix<double, DIM_X, 1>),
                             Matrix<double, DIM_X, 1> input,
                             Matrix<double, DIM_Z, DIM_X>& H) {
@@ -160,13 +152,14 @@ class ExtendedKalmanFilter {
 
     void update(Matrix<double, DIM_U, 1> u,
                 Matrix<double, DIM_Z, 1> z) {
-        calculateJacobianX(transitionFunction,  x, u, G);
+        // Calcuate the Jacobian matrix G using the current state and control
+        calculateJacobianX(transitionFunction, x, u, G);
 
         // State estimation
         x_pred = (*transitionFunction)(x, u);
 
+        // Calcuate the Jacobian matrix H using the predicted state calculated above
         calculateJacobianZ(measurementFunction, x_pred,  H);
-
         x_err_pred = G * x_err * G.transpose() + pred_err;
 
         // Measurement update
@@ -181,8 +174,9 @@ class ExtendedKalmanFilter {
         x_err = x_err_pred;
     }
 
+    // Update using solely state transition prediction with no control and sensation
     void updateWithoutAnything() {
-        // Use Zero control
+        // Use Zero control since we can't explicitly take out the u term
         Matrix<double, DIM_U, 1> u = Matrix<double, DIM_U, 1>::Zero();
         // State estimation
         x_pred = (*transitionFunction)(x, u);
