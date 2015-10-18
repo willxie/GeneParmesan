a2c43693221c99b79a848918eeb93e7c98e5624b
William Xie <wxie@cs.utexas.edu>
2015-10-18 01:20:23 -0500

    Fixed bugs. Added particle generation. Not working tho still

--- core/localization/ParticleFilter.cpp
+++ core/localization/ParticleFilter.cpp
@@ -7,6 +7,13 @@ ParticleFilter::ParticleFilter(MemoryCache& cache, TextLogger*& tlogger)
   : cache_(cache), tlogger_(tlogger), dirty_(true) {
 }
 
+void ParticleFilter::randomParticle(Particle& p) {
+    p.x = (rand() % (2*2500)) - 2500;
+    p.y = (rand() % (2*1250)) - 1250;
+    p.t = rand() % 360;
+    p.w = 0;
+}
+
 void ParticleFilter::init(Point2D loc, float orientation) {
   mean_.translation = loc;
   mean_.rotation = orientation;
@@ -15,13 +22,7 @@ void ParticleFilter::init(Point2D loc, float orientation) {
   srand(time(NULL));
   particles().resize(NUM_PARTICLES);
   for (auto& p : particles()) {
-      p.x = (rand() % (2*2500)) - 2500,
-      p.y = (rand() % (2*1250)) - 1250,
-//      p.x = 0;
-//      p.y = 0;
-      p.t = rand() % 360;
-//      p.t = 0;
-      p.w = 1;
+	  randomParticle(p);
   }
 }
 
@@ -32,30 +33,30 @@ void ParticleFilter::processFrame() {
   // Retrieve odometry update - how do we integrate this into the filter?
   const auto& disp = cache_.odometry->displacement;
   log(41, "Updating particles from odometry: %2.f,%2.f @ %2.2f", disp.translation.x, disp.translation.y, disp.rotation * RAD_T_DEG);
-  
-  // Step each particle deterministically by the odometry
-  for (auto& p : particles()) {
-	  p.t += disp.rotation;
-      p.x += disp.x * cos(p.t);
-      p.y += disp.x * sin(p.t);
-  }
 
-  // Add a little noise to each of the new positions
   std::default_random_engine generator;
   std::normal_distribution<double> x_distribution(0, X_STDDEV);
   std::normal_distribution<double> y_distribution(0, Y_STDDEV);
   std::normal_distribution<double> t_distribution(0, T_STDDEV);
   for (auto& p : particles()) {
+	  // Step each particle deterministically move by the odometry
+	  p.t += disp.rotation;
+      p.x += disp.x * cos(p.t);
+      p.y += disp.x * sin(p.t);
+      p.w = 0;
+
+      // Add a little noise to each of the new positions
+      // Noise should be a function of speed
 	  double x_noise = x_distribution(generator);
 	  double y_noise = y_distribution(generator);
 	  double t_noise = t_distribution(generator);
-
-	  cout << x_noise << " " << y_noise << " " << t_noise << endl;
-
-	  p.x += x_noise;
-	  p.y += y_noise;
+	  if (disp.x != 0) p.x += x_noise;
+	  if (disp.x != 0) p.y += y_noise;
+//	  if (disp.rotation != 0)
 	  p.t += t_noise;
   }
+
+
 
   static vector<WorldObjectType> beacon_ids = {
 		  WO_BEACON_YELLOW_BLUE,
@@ -66,14 +67,23 @@ void ParticleFilter::processFrame() {
 		  WO_BEACON_PINK_BLUE
   };
 
+  double population_quality_total = 0;
+  double population_count = 0;
+  bool beacon_seen = false;
+  WorldObject* lastBeaconPtr;
+
   // Update particle weights with respect to how far they are from the seen beacon
   //
-  // Only take into account the last beacon seen right now
+  // TODO Only take into account the last beacon seen right now
   for (auto& p : particles()) {
 	  for (auto beacon_id : beacon_ids) {
 		  auto& beacon = cache_.world_object->objects_[beacon_id];
-		  if (! beacon.seen)
+		  if (!beacon.seen) {
 			  continue;
+		  }
+
+		  beacon_seen = true;
+		  lastBeaconPtr = &beacon;
 
 		  // Beacon distance
 		  double p_distance = sqrt(pow(abs(p.x-beacon.loc.x), 2) + pow(abs(p.y-beacon.loc.y), 2));
@@ -82,19 +92,37 @@ void ParticleFilter::processFrame() {
 		  double distance_weight = (max_distance - abs(p_distance - nao_distance)) / max_distance;
 
 		  // Orientation
-		  double p_bearing = Point2D(p.x, p.y).getBearingTo(Point2D(beacon.loc), p.t);
-		  double max_bearing_diff = 2 * M_PI;
-		  double bearing_weight = (max_bearing_diff - abs(p_bearing - beacon.visionBearing)) / max_bearing_diff;
+		  double p_bearing = Point2D(p.x, p.y).getBearingTo(beacon.loc, p.t);
+		  double bearing_difference = abs(p_bearing - beacon.visionBearing);
+		  if (bearing_difference > 2 * M_PI) cout << "ERROR Bearing diff: " << p_bearing << ", " << beacon.visionBearing << endl;
+		  if (bearing_difference > M_PI) {
+			  // Angle wrap around
+			  bearing_difference = (2 * M_PI) - bearing_difference;
+		  }
+		  double max_bearing_diff = M_PI;
+		  double bearing_weight = (max_bearing_diff - bearing_difference) / max_bearing_diff;
 
-		  p.w = distance_weight + bearing_weight;
+//		  cout << "beacon coordinate: " << beacon.loc.x  << ", "<< beacon.loc.y << endl;
+//		  cout << "p Bearing: " << p_bearing  << endl;
+		  population_quality_total += (distance_weight + bearing_weight) / 2;
+		  population_count++;
+
+		  const double weight_ratio = 0.9;
+		  p.w += (weight_ratio) * distance_weight + (1 - weight_ratio) * bearing_weight;
 	  }
   }
 
-//  // View particle weights
-//  for (auto& p : particles()) {
-//	  cout << p.w << endl;
-//  }
-//  cout << endl;
+  // If no beacons is seen, don't resample
+  if (!beacon_seen) {
+	  return;
+  }
+
+  // Quality is the best if approaches 1
+  double population_quality_avg = population_quality_total / population_count;
+
+//  cout << "num particles: " << particles().size() << endl;
+  cout << "Population quality: " << population_quality_avg << endl;
+
 
   // Get all particle weights
   vector<double> weights;
@@ -102,11 +130,7 @@ void ParticleFilter::processFrame() {
 	  weights.push_back(p.w);
   }
 
-//  cout << "Weights" << endl;
-//  for (auto weight : weights)
-//	  cout << weight << endl;
-
-   // Sampling machinery
+  // Sampling machinery
   std::random_device rd;
   std::mt19937 gen(rd());
   std::discrete_distribution<> d(weights.begin(), weights.end());
@@ -114,13 +138,35 @@ void ParticleFilter::processFrame() {
   // Resampling
   vector<Particle> winners;
   for (int i = 0; i < NUM_PARTICLES; i++) {
-	  winners.push_back(particles()[d(gen)]);
-  }
+//	  if (i < NUM_PARTICLES * population_quality_avg) {
+	  if (i < NUM_PARTICLES * 0.99) {
+		  winners.push_back(particles()[d(gen)]);
+	  } else {
+		  Particle p;
+		  do {
+			  // Solve for positions based on the last beacon position
+			  float x_sign = -1+2*((float)rand())/RAND_MAX;
+			  float y_sign = -1+2*((float)rand())/RAND_MAX;
 
-  cout << "Winners" << endl;
-  for (auto p : winners)
-	  cout << p.x << " " << p.y << endl;
-  cout << endl;
+			  // Fix x solve y
+			  int x_offset = rand() % static_cast<int>(lastBeaconPtr->visionDistance);
+			  int y_offset = sqrt(pow(lastBeaconPtr->visionDistance, 2) - pow(x_offset, 2));
+
+	//		  cout << lastBeaconPtr->visionDistance << endl;
+	//		  cout << x_offset << ", "<< y_offset << endl;
+			  // Sign
+			  x_sign > 0 ? p.x = lastBeaconPtr->loc.x + x_offset : p.x = lastBeaconPtr->loc.x - x_offset;
+			  y_sign > 0 ? p.y = lastBeaconPtr->loc.y + y_offset : p.y = lastBeaconPtr->loc.y - y_offset;
+		  } while (!(MIN_FIELD_X < p.x && p.x < MAX_FIELD_X &&
+				   MIN_FIELD_Y < p.y && p.y < MAX_FIELD_Y));
+
+		  // Don't need noise because there's already noise in vision
+		  // Random angle
+		  p.t = rand() % 360;
+//		  randomParticle(p);
+		  winners.push_back(p);
+	  }
+  }
 
   particles() = winners;
 }
--- core/localization/ParticleFilter.h
+++ core/localization/ParticleFilter.h
@@ -6,10 +6,15 @@
 #include <memory/LocalizationBlock.h>
 #include <localization/Logging.h>
 
-const int NUM_PARTICLES = 100;
-const double X_STDDEV = 30;
-const double Y_STDDEV = 30;
-const double T_STDDEV = .1;
+const int NUM_PARTICLES = 200;
+const double X_STDDEV = 1;
+const double Y_STDDEV = X_STDDEV;
+const double T_STDDEV = .01;
+
+const int MIN_FIELD_X = -2500;
+const int MAX_FIELD_X = 2500;
+const int MIN_FIELD_Y = -1250;
+const int MAX_FIELD_Y = 1250;
 
 class ParticleFilter {
   public:
@@ -25,6 +30,8 @@ class ParticleFilter {
     inline std::vector<Particle>& particles() {
       return cache_.localization_mem->particles;
     }
+
+    void randomParticle(Particle& p);
 
   private:
     MemoryCache& cache_;
