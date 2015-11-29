15e8ca9dbd89c9f91aa71be0f323985cab9c55cc
Edward Banner <edward.banner@gmail.com>
2015-11-29 15:21:38 -0600

    Reduce keeper.py to minimal behavior

--- core/python/behaviors/keeper.py
+++ core/python/behaviors/keeper.py
@@ -1,211 +1,27 @@
-import core, memory
-import pose, commands, cfgstiff, cfgpose
+import core
+import commands 
 import mem_objects
-from task import Task
 from state_machine import *
-import random
-from memory import joint_commands
-
-class ChangeStiff(Node):
-  OneLegSoft = [0] * core.NUM_JOINTS
-  OneLegSoft[core.HeadYaw] = 0.0
-  OneLegSoft[core.HeadPitch] = 0.0
-  OneLegSoft[core.LShoulderPitch] = 1.0
-  OneLegSoft[core.LShoulderRoll] = 1.0
-  OneLegSoft[core.LElbowYaw] = 1.0
-  OneLegSoft[core.LElbowRoll] = 1.0
-  OneLegSoft[core.LHipYawPitch] = 1.0
-  OneLegSoft[core.LHipPitch] = 1.0
-  OneLegSoft[core.LHipRoll] = 1.0
-  OneLegSoft[core.LKneePitch] = 1.0
-  OneLegSoft[core.LAnklePitch] = 1.0
-  OneLegSoft[core.LAnkleRoll] = 1.0
-  OneLegSoft[core.RHipYawPitch] = 1.0
-  OneLegSoft[core.RHipPitch] = 1.0
-  OneLegSoft[core.RHipRoll] = 1.0
-  OneLegSoft[core.RKneePitch] = 1.0
-  OneLegSoft[core.RAnklePitch] = 1.0
-  OneLegSoft[core.RAnkleRoll] = 1.0
-  OneLegSoft[core.RShoulderPitch] = 1.0
-  OneLegSoft[core.RShoulderRoll] = 1.0
-  OneLegSoft[core.RElbowYaw] = 1.0
-  OneLegSoft[core.RElbowRoll] = 1.0
-
-  def run(self):
-    commands.setStiffness(ChangeStiff.OneLegSoft)
-    if self.getTime() > 20.0:
-      self.finish()
 
 
-class FakeSit(Node):
-  def run(self):
-    pose.Sit()
-    if self.getTime() > 0.1:
-      self.finish()
+class Playing(Node):
+    def run(self):
+        ball = mem_objects.world_objects[core.WO_BALL]
+        commands.setHeadPan(ball.bearing, .3)
+        if not ball.seen:
+            return
 
-class Stand(Node):
-  def run(self):
+        # Turn head to ball
+        ball_x, ball_y = ball.imageCenterX, ball.imageCenterY
+        x_head_turn = -(ball_x-(320.0 / 2.0)) / 160.0
+        commands.setHeadPan(x_head_turn, .5)
 
-    commands.stand()
-    if self.getTime() > 3.0:
-      self.finish()
-
-class BlockLeft(Node):
-  def run(self):
-
-
-    UTdebug.log(15, "Blocking left")
-    if self.getTime() < 1.0:
-      memory.speech.say("Blocking left")
-
-class BlockRight(Node):
-  def run(self):
-    UTdebug.log(15, "Blocking right")
-    if self.getTime() < 1.0:
-      memory.speech.say("Blocking right")
-
-class BlockCenter(Node):
-  def run(self):
-    UTdebug.log(15, "Blocking center")
-    if self.getTime() < 1.0:
-      memory.speech.say("Blocking center")
-
-class NoBlock(Node):
-  def run(self):
-    UTdebug.log(15, "You missed the goal")
-
-    if self.getTime() < 1.0:
-      memory.speech.say("Haha")
-
-class Blocker(Node):
-  def __init__(self):
-    super(Blocker, self).__init__()
-    self.vel_list = [ float(0) ]
-
-  def run(self):
-    if self.getTime() < 1.0:
-      memory.speech.say("Come at me Jake!")
-
-    ball = mem_objects.world_objects[core.WO_BALL]
-    commands.setHeadPan(ball.bearing, 1.0)
-    UTdebug.log(15, ".")
-    # Store a list of previous velocities
-    if ball.seen:
-      UTdebug.log(15, "seen")
-      ball_x, ball_y = ball.imageCenterX, ball.imageCenterY
-      x_head_turn = -(ball_x-(320.0 / 2.0)) / 160.0
-      commands.setHeadPan(x_head_turn, .5)
-
-      # Max size is 6
-      if len(self.vel_list) > 3:
-        self.vel_list = self.vel_list[:-1]
-      temp_list = [ ball.absVel.x ]
-      self.vel_list = temp_list + self.vel_list
-
-      y_intersect = (ball.absVel.y / ball.absVel.x) * (-600 - ball.loc.x) + ball.loc.y
-      UTdebug.log(15, "y_intersect: {}".format(y_intersect))
-
-
-    if ball.distance < 750:
-      # TODO
-      accel = (self.vel_list[-1] - self.vel_list[0]) / float(len(self.vel_list))
-      # ball_loc_pred = abs(ball.absVel.x * 3)
-      t = 3
-      ball_loc_pred = (ball.absVel.x * t) + ((t * t) / 2 * accel)
-      # if abs(ball.absVel.x * 3) > ball.distance:
-      if True:
         # Find left or right or center block
         y_intersect = (ball.absVel.y / ball.absVel.x) * (-750 - ball.loc.x) + ball.loc.y
 
-        UTdebug.log(15, "distance: {}\t ball_loc_pred: {}\t ball.absVel.x: {}".format(ball.distance, ball_loc_pred, ball.absVel.x))
-        UTdebug.log(15, "accel: {}".format(accel))
-        UTdebug.log(15, "Ball is close, blocking!")
-        print("y_intersect: {}".format(y_intersect))
+        UTdebug.log(15, 'y intercept: {}'.format(y_intersect))
 
-        xvel_avg = sum(self.vel_list) / len(self.vel_list)
-        print('XVEL_LENGTH = {}'.format(len(self.vel_list)))
-        print('XVEL_AVG = {}'.format(xvel_avg))
-
-        min_xvel_avg = -650
-
-        # if ball.bearing > 30 * core.DEG_T_RAD:
-        if xvel_avg < min_xvel_avg:
-          if 130 < y_intersect < 500:
-            choice = "left"
-            # elif ball.bearing < -30 * core.DEG_T_RAD:
-          elif -140 < y_intersect < 130:
-            choice = "center"
-          elif -500 < y_intersect < -140:
-            choice = "right"
-          # else:
-          #   choice = "no_block"
-          self.vel_list = []
-          self.postSignal(choice)
+        if -500 < y_intersect < 500:
+            UTdebug.log(15, 'Shot!')
         else:
-          self.vel_list = []
-
-
-class Playing(LoopingStateMachine):
-  def setup(self):
-    # commands.stand()
-    blocker = Blocker()
-    blocks = {
-      "left": BlockLeft(),
-      "right": BlockRight(),
-      "center": BlockCenter(),
-      "no_block": NoBlock()
-    }
-
-    standingLeftArmPose = dict()
-    # standingLeftArmPose[core.LShoulderPitch] = 0
-    standingLeftArmPose[core.LShoulderRoll] = 90
-    # standingLeftArmPose[core.LElbowYaw] = -0
-    # standingLeftArmPose[core.LElbowRoll] = -0
-
-    standingRightArmPose = dict()
-    # standingRightArmPose[core.RShoulderPitch] = 0
-    standingRightArmPose[core.RShoulderRoll] = 90
-    # standingRightArmPose[core.RElbowYaw] = -0
-    # standingRightArmPose[core.RElbowRoll] = -0
-
-    standingBothArmPose = dict()
-    # standingBothArmPose[core.RShoulderPitch] = 1
-    standingBothArmPose[core.RShoulderRoll] = 90
-    # standingBothArmPose[core.RElbowYaw] = -0
-    # standingBothArmPose[core.RElbowRoll] = -0
-    # standingBothArmPose[core.LShoulderPitch] = 1
-    standingBothArmPose[core.LShoulderRoll] = 90
-    # standingBothArmPose[core.LElbowYaw] = -0
-    # standingBothArmPose[core.LElbowRoll] = -0
-
-    # for i in range(2, core.NUM_JOINTS):
-    #   val = util.getPoseJoint(i, standingLeftArmPose, False)
-    #   if val != None:
-    #     joint_commands.setJointCommand(i, val * core.DEG_T_RAD)
-
-    toPoseTime = 0.2
-    poses = {
-      "left": pose.ToPose(standingLeftArmPose, toPoseTime),
-      "right": pose.ToPose(standingRightArmPose, toPoseTime),
-      "center": pose.ToPose(standingBothArmPose, toPoseTime),
-      "no_block": pose.ToPose(cfgpose.standingPose, toPoseTime)
-    }
-
-    stand = Stand()
-
-    self.trans(stand, C, pose.ToPose(cfgpose.standingPose), C, blocker)
-
-    for name in blocks:
-      b = blocks[name]
-      p = poses[name]
-      self.trans(blocker, S(name), p, C, b, T(3), pose.ToPose(cfgpose.standingPose, toPoseTime), C, blocker)
-
-    self.setFinish(None) # This ensures that the last node in trans is not the final node
-
-class Set(StateMachine):
-  def setup(self):
-    # Movements
-    stand = Stand()
-    fake_sit = FakeSit()
-    change_stiff = ChangeStiff()
-    self.trans(stand, C, fake_sit, C, change_stiff)
+            UTdebug.log(15, 'No Shot!')
